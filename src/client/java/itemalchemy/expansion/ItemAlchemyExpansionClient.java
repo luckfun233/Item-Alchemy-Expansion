18c02df95a83763363bf5cf38cf257e8f8dc1511
luckfun233 <231908363+luckfun233@users.noreply.github.com>
2026-08-09 07:20:29 +0800

    init: Item-Alchemy-Expansion for MC 1.21.1

---
+++ src/client/java/itemalchemy/expansion/ItemAlchemyExpansionClient.java
@@ -0,0 +1,10 @@
+package itemalchemy.expansion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemAlchemyExpansionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
